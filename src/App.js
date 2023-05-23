1c0feafcab4317b06f00f5258d89ff345ad77acb
anamponsah <anamponsah2@gmail.com>
2023-05-23 11:07:40 +0000

    frontend schema

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="sub">
+        <form>
+          <label for = "email">email</label>
+          <input type="email" placeholder='youremail@gmail.com'></input>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
